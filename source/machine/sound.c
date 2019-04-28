2ac7a4f064a43bc7b6221884d7d9a22abaf53b61
agirre <jon.agirre@york.ac.uk>
2019-04-28 19:26:55 +0100

    Initial commit, basic structure there although Makefile not linked to actual files

---
+++ source/machine/sound.c
@@ -0,0 +1,7 @@
+// A very simple videogame project to get my daughter started with coding
+// for bare-metal platforms. I shall try and keep all machine-dependent source
+// together and separate so we can port the project 'easily'
+// Authors: Jon Agirre & Iraia Agirre, 2019
+// Licence: GPL v3.0 (https://www.gnu.org/licenses/gpl.html)
+//
+// The sprites are generated with: bmp2raw -x name.bmp name.raw
